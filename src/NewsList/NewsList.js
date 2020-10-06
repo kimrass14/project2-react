49e4d37b4ffd0a8815e225333b461ee7696841f3
Kim Rasmussen <kim@Kims-MacBook-Pro.local>
2020-10-06 15:58:09 -0500

    oct6 - preview responsiveness

--- src/NewsList/NewsList.js
+++ src/NewsList/NewsList.js
@@ -15,15 +15,18 @@ const NewsList = (props) => {
     let showNews = ''
     if (props.list[0]) {
         showNews = props.list.map((item, index) => {
+            
             return (
                 <div className="preview" key={index} onClick={() => { handleClick(item.url) }}>
-                    <img className="image" src={item.urlToImage} alt="newsimage" />
+                    <img className="image" src={item.urlToImage !== null ? item.urlToImage : "https://source.unsplash.com/aId-xYRTlEc"} alt="newsimage" />
                     <div className="article-info">
                         <p className="title">{item.title}</p>
-                        <p className="source">{item.source.name}</p>
+                        {/* <p className="source">{item.source.name}</p> */}
+                        <p className="description">{item.description}</p>
                     </div>
                 </div>
             )
+            
         })
     }
 
